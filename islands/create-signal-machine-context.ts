55f72dd1ea9038a522b59dda543de5406e18728f
nikita <nik.moiseienko@gmail.com>
2024-02-03 18:23:15 +0100

    it looks like working... but too many questions and still need developing

--- islands/create-signal-machine-context.ts
+++ islands/create-signal-machine-context.ts
@@ -1,5 +1,4 @@
 import { effect, Signal, signal, useSignal } from "@preact/signals";
-import { genLogger } from "../utils/gen-logger.ts";
 import {
   Actor,
   ActorOptions,
@@ -7,14 +6,6 @@ import {
   createActor,
   SnapshotFrom,
 } from "xstate";
-
-const log = genLogger({
-  level: "debug",
-  name: "createSignalMachineContext",
-  margin_left: 20,
-});
-
-let _id = 0;
 
 export function createSignalMachineContext<TLogic extends AnyActorLogic>(
   machine: TLogic,
@@ -24,25 +15,16 @@ export function createSignalMachineContext<TLogic extends AnyActorLogic>(
     selector: (snapshot: SnapshotFrom<TLogic>) => T,
   ) => Signal<T>;
   actor: Actor<TLogic>;
-  [Symbol.dispose]: () => void;
 } {
-  log();
   const actor = signal(createActor(machine, options));
   const subscribers = signal<
     Set<(snapshot: SnapshotFrom<TLogic>) => unknown>
   >(
     new Set(),
   );
-  const cleaner = signal(
-    new Map<
-      number,
-      ((snapshot: SnapshotFrom<TLogic>) => void)[]
-    >(),
-  );
+
   effect(() => {
-    log("useSignalEffect()");
     const subscription = actor.peek().subscribe((s) => {
-      log("cb inside actor.subscribe", s);
       for (const cb of subscribers.peek()) cb(s);
     });
     actor.peek().start();
@@ -51,31 +33,18 @@ export function createSignalMachineContext<TLogic extends AnyActorLogic>(
       subscription.unsubscribe();
     };
   });
-  const id = ++_id;
-  const result = {
+
+  return {
     signalizeSelector<T>(cb: (s: SnapshotFrom<TLogic>) => T) {
       const signalization = useSignal(cb(actor.peek().getSnapshot()));
       const _cb = (s: SnapshotFrom<TLogic>) => {
-        signalization.value = cb(s);
+        const currentValue = cb(s);
+        signalization.value = currentValue;
       };
       subscribers.peek().add(_cb);
-      const _cleaner = cleaner.peek();
-      const prev = _cleaner.get(id);
-      if (prev) {
-        prev.push(_cb);
-      } else {
-        _cleaner.set(id, [_cb]);
-      }
+
       return signalization;
     },
     actor: actor.peek(),
-    [Symbol.dispose]() {
-      log("dispose!");
-      cleaner.peek().get(id)?.forEach((cb) => {
-        subscribers.peek().delete(cb);
-      });
-    },
   };
-
-  return result;
 }
