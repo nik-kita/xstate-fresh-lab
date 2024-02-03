2155ca244ee2e196834c9e0a67e451231569577a
nikita <nik.moiseienko@gmail.com>
2024-02-03 20:00:27 +0100

    first minimal version

--- islands/Traffic.tsx
+++ islands/Traffic.tsx
@@ -1,9 +1,7 @@
 import { TrafficContext } from "./traffic-machine-context.ts";
 
 export default function Traffic() {
-  console.log("render!");
   const {
-    actor,
     signalizeSelector,
   } = TrafficContext;
   const trafficState = signalizeSelector((snapshot) => {
@@ -11,9 +9,27 @@ export default function Traffic() {
   });
 
   return (
-    <div>
-      Traffic
-      <pre>{trafficState}</pre>
+    <div class={"flex gap-4 bg-black p-2 rounded-sm"}>
+      <div
+        class={`border-2 rounded-full h-10 w-10 ${
+          trafficState.value === "Red" ? "bg-red-600" : "bg-red-950"
+        }`}
+      >
+      </div>
+      <div
+        class={`border-2 rounded-full h-10 w-10 ${
+          trafficState.value !== "Red" && trafficState.peek() !== "Green"
+            ? "bg-yellow-400"
+            : "bg-yellow-900"
+        }`}
+      >
+      </div>
+      <div
+        class={`border-2 rounded-full h-10 w-10 ${
+          trafficState.value === "Green" ? "bg-green-500" : "bg-green-950"
+        }`}
+      >
+      </div>
     </div>
   );
 }
