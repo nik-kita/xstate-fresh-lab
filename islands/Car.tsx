0be309d091f41edd0bd35136a7a398e2f26f6cbb
nikita <nik.moiseienko@gmail.com>
2024-02-05 18:52:59 +0100

    car

--- islands/Car.tsx
+++ islands/Car.tsx
@@ -2,4 +2,18 @@ import { MachineContext } from "./traffic-machine-context.ts";
 
 export default function Car() {
   const { useSelector } = MachineContext;
+  const car = useSelector((s) => {
+    return s.value.car;
+  });
+  return (
+    <div
+      class={car.value === "move"
+        ? "bg-green-500"
+        : car.value === "stop"
+        ? "bg-red-500"
+        : "bg-blue-600"}
+    >
+      {car.value}
+    </div>
+  );
 }
