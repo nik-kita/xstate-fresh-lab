1df5a1ff3fe94a5c33d35a6f2669f24130a05567
nikita <nik.moiseienko@gmail.com>
2024-02-05 18:53:34 +0100

    UiWrapper IS_BROWSER to prevent sidee-effects  on servvver

--- islands/UiWrapper.tsx
+++ islands/UiWrapper.tsx
@@ -1,15 +1,20 @@
+import { IS_BROWSER } from "$fresh/runtime.ts";
 import { ComponentChildren } from "preact";
 import { MachineContext } from "./traffic-machine-context.ts";
 
 export default function UiWrapper(
   { children }: { children: ComponentChildren },
 ) {
+  if (!IS_BROWSER) {
+    return <></>;
+  }
+
   const { start } = MachineContext;
 
   start();
   return (
-    <div>
+    <>
       {children}
-    </div>
+    </>
   );
 }
